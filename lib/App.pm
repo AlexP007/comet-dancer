3af38ecd31099553f578eabc5fc50f24ce3e6029
Alexander Panteleev <alexpan@cpan.org>
2022-03-25 18:39:02 +0300

    [i] routes
    Took 7 minutes

--- lib/App.pm
+++ lib/App.pm
@@ -8,5 +8,14 @@ use App::Http::Controllers::Login;
 
 our $VERSION = '0.1';
 
+hook before_template_render => sub {
+    my $tokens = shift;
+
+    $tokens->{routes} = {
+        'login'    => '/login',
+        'forget'   => '/forget',
+        'register' => '/register',
+    };
+};
 
 true;
