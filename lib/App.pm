ae22e6b92dc486352f3a75e5e7bf3e24a7d00459
Alexander Panteleev <alexpan@cpan.org>
2022-03-25 12:47:11 +0300

    [i] Loging controller
    Took 10 minutes

--- lib/App.pm
+++ lib/App.pm
@@ -4,13 +4,9 @@ use Dancer2;
 use Dancer2::Plugin::DBIC;
 use Dancer2::Plugin::Auth::Extensible;
 
+use App::Http::Controllers::Login;
+
 our $VERSION = '0.1';
 
-get '/' => sub {
-    template 'app/login' => {
-        title  => 'ho',
-        lang   => 'app',
-    };
-};
 
 true;
---
+++ lib/App/Http/Controllers/Login.pm
@@ -0,0 +1,11 @@
+package App::Http::Controllers::Login;
+
+use Dancer2 appname  =>'App';
+
+get '/login' => sub {
+    template 'app/login' => {
+        title  => 'login',
+    };
+};
+
+1;
