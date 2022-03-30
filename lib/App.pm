061c17861db970de702e1cbc4a129e039bd0efe9
Alexander Panteleev <alexpan@cpan.org>
2022-03-30 23:47:13 +0300

    [r] removed unnecessary code from App
    Took 27 seconds

--- lib/App.pm
+++ lib/App.pm
@@ -1,21 +1,8 @@
 package App;
 
 use Dancer2;
-use Dancer2::Plugin::DBIC;
-use Dancer2::Plugin::Auth::Extensible;
-
 use App::Http::Controllers::Login;
 
 our $VERSION = '0.1';
-
-hook before_template_render => sub {
-    my $tokens = shift;
-
-    $tokens->{routes} = {
-        'login'    => '/login',
-        'forget'   => '/forget',
-        'register' => '/register',
-    };
-};
 
 true;
