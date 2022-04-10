cbf890847a9916f06827b51861bb56fd0cbce873
Alexander Panteleev <alexpan@cpan.org>
2022-04-10 19:16:09 +0300

    [i] role table
    Took 17 minutes

--- lib/Admin/Http/Controllers/Dashboard.pm
+++ lib/Admin/Http/Controllers/Dashboard.pm
@@ -1,6 +1,7 @@
 package Admin::Http::Controllers::Dashboard;
 
 use Dancer2 appname  =>'Admin';
+use Dancer2::Plugin::DBIC;
 
 get '/dashboard' => sub {
     template 'admin/dashboard/index', {
@@ -17,6 +18,15 @@ get '/dashboard/users' => sub {
 get '/dashboard/users/create' => sub {
     template 'admin/dashboard/users_create', {
         title => 'Create user',
+    }
+};
+
+get '/dashboard/users/groups' => sub {
+    my @roles = rset('Role')->all;
+
+    template 'admin/dashboard/users_roles' , {
+        title => 'Roles',
+        roles => \@roles,
     }
 };
 
